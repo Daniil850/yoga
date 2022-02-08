4beae5bcacebe1b4e97e2a5314141e2fce96b73a
Daniil850 <daniilzagorodny029@gmail.com>
2022-02-08 12:50:05 +0300

    end of yoga

--- yogaZagorodnyZabl WatchKit Extension/InterfaceController.swift
+++ yogaZagorodnyZabl WatchKit Extension/InterfaceController.swift
@@ -4,15 +4,35 @@
 //
 //  Created by Даниил on 08.02.2022.
 //
-
 import WatchKit
 import Foundation
+import WatchConnectivity
 
 
-class InterfaceController: WKInterfaceController {
-
+class InterfaceController: WKInterfaceController, WCSessionDelegate {
+    
+    var wcSession: WCSession!
+    
+    @IBOutlet weak var name: WKInterfaceLabel!
+    @IBOutlet weak var image: WKInterfaceImage!
+    @IBOutlet weak var timer: WKInterfaceTimer!
+    
     override func awake(withContext context: Any?) {
-        // Configure interface objects here.
+        wcSession = WCSession.default
+        wcSession.delegate = self
+        wcSession.activate()
+    }
+    
+    func session(_ session: WCSession, didReceiveApplicationContext applicationContext: [String : Any]) {
+        let message = applicationContext["name"] as? String
+        let time = applicationContext["time"] as? Double
+        name.setText(message)
+        timer.setDate(NSDate(timeIntervalSinceNow: time!) as Date)
+        timer.start()
+    }
+    
+    func session (_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        
     }
     
     override func willActivate() {
